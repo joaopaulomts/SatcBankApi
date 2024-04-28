8004637dfca77709dc361f0aa36d20b4c0373c71
Edvan Ronchi <edvan_ronchi@hotmail.com>
2024-04-28 02:53:43 -0300

    #:feat: Adicionado migrations.

---
+++ Presentations/Migrations/20240428015543_InitialCreate.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Presentations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: false),
+                    Senha = table.Column<string>(type: "TEXT", nullable: false),
+                    Saldo = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deposito",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Data = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false),
+                    UsuarioId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deposito", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Deposito_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transferencia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Data = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false),
+                    UsuarioOrigemId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UsuarioDestinoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transferencia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transferencia_Usuario_UsuarioDestinoId",
+                        column: x => x.UsuarioDestinoId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transferencia_Usuario_UsuarioOrigemId",
+                        column: x => x.UsuarioOrigemId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deposito_UsuarioId",
+                table: "Deposito",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transferencia_UsuarioDestinoId",
+                table: "Transferencia",
+                column: "UsuarioDestinoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transferencia_UsuarioOrigemId",
+                table: "Transferencia",
+                column: "UsuarioOrigemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deposito");
+
+            migrationBuilder.DropTable(
+                name: "Transferencia");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
